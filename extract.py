6f7783deeb20e263f7799e0fad681c4d39c6ac3d
sandeep <vattyam.sandeep@gmail.com>
2024-12-18 21:05:56 +0530

    added db changes

--- extract.py
+++ extract.py
@@ -1,9 +1,13 @@
 import datetime, time
 import requests
 import selectorlib
+import sqlite3
 
 
 URL = "http://programmer100.pythonanywhere.com/"
+
+
+connection = sqlite3.connect("data.db")
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -18,21 +22,38 @@ def extract(source):
     return value
 
 
+# def store(extracted):
+#     with open("data.txt", "a") as file:
+#         file.write(str(datetime.datetime.now()) + "," + extracted + "\n")
+
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(str(datetime.datetime.now()) + "," + extracted + "\n")
+    now = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperature_data VALUES(?,?)",
+                   (now,extracted))
+    connection.commit()
+
 
 #def read(extracted):
 #    with open("data.txt", "r") as file:
  #       return file.read()
 
+# def read(extracted):
+#     row = extracted.split(",")
+#     date, temperature = row
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT * FROM temperature_data WHERE date=? AND temperature=?",
+#                    (date, temperature))
+#     rows = cursor.fetchall()
+#     return rows
+
 if __name__ == "__main__":
-    while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
-        print(extracted)
-        store(extracted)
-        time.sleep(2)
+#    while True:
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    store(extracted)
+ #       time.sleep(2)
  #       content = read(extracted)
  #       if extracted != "No upcoming tours":
  #           if extracted not in content:
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+import sqlite3
 
-df = pd.read_csv("data.txt")
-figure = px.line(x=df["date"], y=df["temperature"],
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
+cursor.execute("SELECT date FROM temperature_data")
+date = cursor.fetchall()
+date = [item[0] for item in date]
+#df = pd.read_csv("data.txt")
+
+
+cursor.execute("SELECT temperature FROM temperature_data")
+temperature = cursor.fetchall()
+temperature = [item[0] for item in temperature]
+
+# figure = px.line(x=df["date"], y=df["temperature"],
+#                  labels={"x":"Date", "y": "Temperature (C)"})
+
+figure = px.line(x=date, y=temperature,
                  labels={"x":"Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
